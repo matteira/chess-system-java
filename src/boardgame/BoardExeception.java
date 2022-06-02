aa49c0d47d7e07b0906feb716bd274640a0c8330
Gabriel Mattos <gabrielmattoscosta4@gmail.com>
2022-06-02 12:00:26 -0300

    Project Final

--- src/boardgame/BoardExeception.java
+++
@@ -1,9 +0,0 @@
-package boardgame;
-
-public class BoardExeception extends RuntimeException{
-    public static final long serialVersionUID = 1l;
-
-    public BoardExeception(String msg) {
-        super(msg);
-    }
-}
--- src/chess/ChessExeception.java
+++
@@ -1,9 +0,0 @@
-package chess;
-
-public class ChessExeception extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public ChessExeception(String msg) {
-        super(msg);
-    }
-}
